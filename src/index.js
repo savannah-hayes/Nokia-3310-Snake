aecb0a7a04bbe4fe10de295d8368c8d130f7bd75
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-05-24 15:54:22 +0200

    Defined variables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const squares = document.querySelectorAll(".grid div");
+  const scoreDisplay = document.querySelector("span");
+  const startButton = document.querySelector(".startButton");
 
+  const width = 10;
+  let currentIndex = 0;
+  let appleIndex = 0;
+  let currentSnake = [2, 1, 0];
+  let direction = 1;
+  let score = 0;
+  let speed = 0.9;
+  let intervalTime = 0;
+  let interval = 0;
+});
