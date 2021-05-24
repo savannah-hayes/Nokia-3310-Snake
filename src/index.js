9ceb5e78167d944161faed03aef55a650e05fa46
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-05-24 19:41:06 +0200

    Added functions to index.js

--- src/index.js
+++ src/index.js
@@ -12,4 +12,72 @@ document.addEventListener("DOMContentLoaded", () => {
   let speed = 0.9;
   let intervalTime = 0;
   let interval = 0;
+
+  function startGame() {
+    currentSnake.forEach((index) => squares[index].classList.remove("snake"));
+    squares[appleIndex].classList.remove("apple");
+    clearInterval(interval);
+    score = 0;
+    randomApple();
+    direction = 1;
+    scoreDisplay.innerText = score;
+    intervalTime = 1000;
+    currentSnake = [2, 1, 0];
+    currentIndex = 0;
+    currentSnake.forEach((index) => squares[index].classList.add("snake"));
+    interval = setInterval(moveOutcomes, intervalTime);
+  }
+
+  function moveOutcomes() {
+    if (
+      (currentSnake[0] + width >= width * width && direction === width) ||
+      (currentSnake[0] % width === width - 1 && direction === 1) ||
+      (currentSnake[0] % width === 0 && direction === -1) ||
+      (currentSnake[0] - width < 0 && direction === -width) ||
+      squares[currentSnake[0] + direction].classList.contains("snake")
+    ) {
+      return clearInterval(interval);
+    }
+
+    const tail = currentSnake.pop();
+    squares[tail].classList.remove("snake");
+    currentSnake.unshift(currentSnake[0] + direction);
+
+    if (squares[currentSnake[0]].classList.contains("apple")) {
+      squares[currentSnake[0]].classList.remove("apple");
+      squares[tail].classList.add("snake");
+      currentSnake.push(tail);
+      randomApple();
+      score++;
+      scoreDisplay.textContent = score;
+      clearInterval(interval);
+      intervalTime = intervalTime * speed;
+      interval = setInterval(moveOutcomes, intervalTime);
+    }
+    squares[currentSnake[0]].classList.add("snake");
+  }
+
+  function randomApple() {
+    do {
+      appleIndex = Math.floor(Math.random() * squares.length);
+    } while (squares[appleIndex].classList.contains("snake"));
+    squares[appleIndex].classList.add("apple");
+  }
+
+  function control(event) {
+    squares[currentIndex].classList.remove("snake");
+
+    if (event.keyCode === 39) {
+      direction = 1;
+    } else if (event.keyCode === 38) {
+      direction = -width;
+    } else if (event.keyCode === 37) {
+      direction = -1;
+    } else if (event.keyCode === 40) {
+      direction = +width;
+    }
+  }
+
+  document.addEventListener("keyup", control);
+  startButton.addEventListener("click", startGame);
 });
